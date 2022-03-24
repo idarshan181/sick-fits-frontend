76905e58c7af04fa7bfec8f6a98795207bdbf684
Darshan Patel <ddpatel97@gmail.com>
2022-03-25 01:42:11 +0530

    Added: graphQL Query to fetch products and display using reusable component

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,5 +1,8 @@
+import Image from 'next/image';
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Product from './Product';
 
 interface ProductsInterface {
   id: string;
@@ -34,12 +37,25 @@ const ALL_PRODUCTS_QUERY = gql`
     }
   }
 `;
+
+const ProductsList = styled.div`
+  display: grid;
+  /* grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); */
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { loading, data, error } = useQuery<ProductsData>(ALL_PRODUCTS_QUERY);
   if (loading) return <p>Loading...</p>;
+  if (error) return <p> Error: {error.message} </p>;
   return (
     <div>
-      <p>Products Component!!</p>
+      <ProductsList>
+        {data.allProducts.map((product: ProductsInterface) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductsList>
     </div>
   );
 }
